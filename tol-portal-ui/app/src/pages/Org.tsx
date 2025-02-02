7e5b8499d2e976f90f85edcc5930ed0a4ad6e5b2
Edward Moulsdale <em29@sanger.ac.uk>
2025-02-02 18:48:33 +0000

    finished

--- tol-portal-ui/app/src/pages/Org.tsx
+++ tol-portal-ui/app/src/pages/Org.tsx
@@ -7,6 +7,8 @@
 import React, {useEffect, useState} from 'react';
 import { Tree, TreeNode } from 'react-organizational-chart';
 
+import { HoverOverlay } from '@tol/tol-ui';
+
 interface OrgTreeNode {
   id: Number
   name: string
@@ -23,15 +25,27 @@ export default function Org() {
 
     return (underSelected === true) ? 'org-node org-node-under': 'org-node';
   }
-  
+ 
+  const hoverNode = (nodeId: Number, children): HoverOverlay => (
+    <HoverOverlay
+      children={children}
+      contents={<div>{nodeId}</div>}
+    >
+    </HoverOverlay>
+  );
+
   const dumpNode = (orgNode: OrgTreeNode, underSelected: boolean = false): TreeNode => {
     const isSelected = selected === orgNode.id;
-    console.log(orgNode.id, isSelected)
-  
     const nodeClass = getNodeClass(isSelected, underSelected);
+  
+    const children = (
+      <div className={nodeClass} onClick={() => updateSelected(orgNode.id)}>{orgNode.name}</div>
+    )
+
+    const label: HoverOverlay = hoverNode(orgNode.id, children)
 
     return (
-      <TreeNode key={orgNode.id} label={<div className={nodeClass} onClick={() => updateSelected(orgNode.id)}>{orgNode.name}</div>}>
+      <TreeNode key={orgNode.id} label={label}>
         {
           orgNode.children.map(
             childNode => dumpNode(childNode, isSelected || underSelected)
@@ -61,11 +75,18 @@ export default function Org() {
     []
   );
 
+  const rootLabel = rootNode === null ? null : hoverNode(
+    rootNode.id,
+    (
+      <div className="org-node org-node-root" onClick={resetSelected}>Sanger</div>
+    )
+  );
+
   return rootNode === null ? (<h1>Loading....</h1>) : (
-    <Tree label={<div className="org-node org-node-root" onClick={resetSelected}>Sanger</div>} key={rootNode.id}>
+    <Tree label={rootLabel} key={rootNode.id}>
       {
         rootNode.children.map(dumpNode)
       }
     </Tree>
-  )
+  );
 }
