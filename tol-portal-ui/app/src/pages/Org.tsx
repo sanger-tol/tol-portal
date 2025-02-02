b86d2eb0d4de390e7c56dc74eb6d896e4870e837
Edward Moulsdale <em29@sanger.ac.uk>
2025-02-02 18:17:15 +0000

    business logic

--- tol-portal-ui/app/src/pages/Org.tsx
+++ tol-portal-ui/app/src/pages/Org.tsx
@@ -8,7 +8,7 @@ import React, {useEffect, useState} from 'react';
 import { Tree, TreeNode } from 'react-organizational-chart';
 
 interface OrgTreeNode {
-  id: string
+  id: Number
   name: string
   children: OrgTreeNode[]
 }
@@ -16,16 +16,29 @@ interface OrgTreeNode {
 export default function Org() {
 
   const [rootNode, setRootNode] = useState<OrgTreeNode | null>(null);
+  const [selected, setSelected] = useState<Number | null>(null);
   
-  const dumpNode = (orgNode: OrgTreeNode): TreeNode => (
-    <TreeNode key={orgNode.id} label={<div>{orgNode.name}</div>}>
-      {
-        orgNode.children.map(
-          childNode => dumpNode(childNode)
-        )
-      }
-    </TreeNode>
-  );
+  const dumpNode = (orgNode: OrgTreeNode, underSelected: boolean = false): TreeNode => {
+    const isSelected = selected === orgNode.id;
+
+    return (
+      <TreeNode key={orgNode.id} label={<div onClick={() => updateSelected(orgNode.id)}>{orgNode.name}</div>}>
+        {
+          orgNode.children.map(
+            childNode => dumpNode(childNode, underSelected || isSelected)
+          )
+        }
+      </TreeNode>
+    );
+  }
+
+  const resetSelected = (): void => setSelected(null);
+
+  const updateSelected = (updated: string): void => {
+    console.log(updated);
+    if (updated === selected) return resetSelected();
+    setSelected(updated);
+  };
 
   useEffect(
     () => fetch(
@@ -40,7 +53,7 @@ export default function Org() {
   );
 
   return rootNode === null ? (<h1>Loading....</h1>) : (
-    <Tree label={<div>Sanger</div>} key={rootNode.id}>
+    <Tree label={<div onClick={resetSelected}>Sanger</div>} key={rootNode.id}>
       {
         rootNode.children.map(dumpNode)
       }
