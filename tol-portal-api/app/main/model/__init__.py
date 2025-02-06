0cc1dd9ffc368acb76166a91c6b837da3fc68ec1
Edward Moulsdale <em29@sanger.ac.uk>
2025-02-06 15:56:59 +0000

    put more back

--- tol-portal-api/app/main/model/__init__.py
+++ tol-portal-api/app/main/model/__init__.py
@@ -5,11 +5,15 @@
 from .action import Action
 from .base import Base  # noqa
 from .data_load_event import DataLoadEvent
+<<<<<<< HEAD
 from .extraction_event import ExtractionEvent
 from .sample_event import SampleEvent
 from .sequencing_request_event import SequencingRequestEvent
 from .tissue_prep_event import TissuePrepEvent
 from .tolid_event import TolidEvent
+=======
+from .sequencing_request_event import SequencingRequestEvent
+>>>>>>> 0fec837 (put more back)
 from .user_action import UserAction
 from .user_mixin import UserMixin  # noqa
 
@@ -17,10 +21,14 @@ from .user_mixin import UserMixin  # noqa
 MODELS = (
     Action,
     DataLoadEvent,
+<<<<<<< HEAD
     ExtractionEvent,
     SampleEvent,
     SequencingRequestEvent,
     TissuePrepEvent,
     TolidEvent,
+=======
+    SequencingRequestEvent,
+>>>>>>> 0fec837 (put more back)
     UserAction
 )
