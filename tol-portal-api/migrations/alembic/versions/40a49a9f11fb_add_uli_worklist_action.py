8a15a618b8c520e349133125f1582d8ab79171d0
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-02-20 14:33:04 +0000

    Setting flow for action options

---
+++ tol-portal-api/migrations/alembic/versions/40a49a9f11fb_add_uli_worklist_action.py
@@ -0,0 +1,40 @@
+"""add uli worklist action
+
+Revision ID: 40a49a9f11fb
+Revises: e2ece2f28e88
+Create Date: 2025-02-20 14:17:21.229424
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '40a49a9f11fb'
+down_revision = 'e2ece2f28e88'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    connection = op.get_bind()
+    session = Session(bind=connection)
+
+    session.execute(
+            sa.text(
+                """
+                INSERT INTO action ("name", object_type, flow_name, params)
+                VALUES (
+                    'ULI Work List',
+                    'extraction',
+                    'elastic_tum_benchling',
+                    '{"worklist_name": "DNA ready for ULI pacbio prep"}'::JSONB
+                );
+                """
+            )
+        )
+
+
+
+def downgrade() -> None:
+    pass
