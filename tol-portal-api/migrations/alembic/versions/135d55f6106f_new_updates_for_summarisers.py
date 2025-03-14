67b29a908226e2650c02e11614ae24accd295642
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-03-14 12:33:07 +0000

    New species_event actions

---
+++ tol-portal-api/migrations/alembic/versions/135d55f6106f_new_updates_for_summarisers.py
@@ -0,0 +1,24 @@
+"""new updates for summarisers
+
+Revision ID: 135d55f6106f
+Revises: 622ae94b2d4a
+Create Date: 2025-03-26 11:20:24.956119
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '135d55f6106f'
+down_revision = '622ae94b2d4a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Fix field prefix for tolid summariser
+    op.execute("UPDATE summary SET prefix = 'calc' WHERE source_object_type = 'sequencing_request' AND destination_object_type = 'tolid' AND prefix = 'benchling' AND stats_fields::text LIKE '%calc_mlwh_volume_remaining%'")
+
+def downgrade() -> None:
+    pass
