190ced2022f7b25bf7eae6a767ea834255a72aa4
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-03-14 13:21:06 +0000

    lint

--- tol-portal-api/app/main/model/__init__.py
+++ tol-portal-api/app/main/model/__init__.py
@@ -7,9 +7,9 @@ from .data_load_event import DataLoadEvent
 from .extraction_event import ExtractionEvent
 from .sample_event import SampleEvent
 from .sequencing_request_event import SequencingRequestEvent
+from .species_event import SpeciesEvent
 from .tissue_prep_event import TissuePrepEvent
 from .tolid_event import TolidEvent
-from .species_event import SpeciesEvent
 
 
 MODELS = (
