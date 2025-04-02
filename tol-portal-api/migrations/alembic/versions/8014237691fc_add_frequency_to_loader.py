b2fa8db580cab365aa9cc1e83ad86df9d75ec633
Andrew Varley <av10@sanger.ac.uk>
2025-04-02 16:53:42 +0100

    TOLP-8578 Add frequency to loaders

---
+++ tol-portal-api/migrations/alembic/versions/8014237691fc_add_frequency_to_loader.py
@@ -0,0 +1,37 @@
+"""Add frequency to loader
+
+Revision ID: 8014237691fc
+Revises: ef0cfb361373
+Create Date: 2025-04-02 10:06:11.543106
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8014237691fc'
+down_revision = 'ef0cfb361373'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'loader',
+        sa.Column('frequency', sa.String, nullable=False, server_default='daily')
+    )
+    op.execute(
+        sa.text(
+            "UPDATE loader SET frequency = 'quarter_hourly' WHERE id in (3)"
+        )
+    )
+    op.execute(
+        sa.text(
+            "UPDATE loader SET frequency = 'hourly' WHERE id in (11)"
+        )
+    )
+
+
+def downgrade() -> None:
+    pass
