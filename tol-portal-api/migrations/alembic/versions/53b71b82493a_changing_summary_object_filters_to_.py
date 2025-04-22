2f232c9cd3c00bc47e94c3236afbd0e5e294454b
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-04-22 09:16:45 +0100

    UAT-based refinements

---
+++ tol-portal-api/migrations/alembic/versions/53b71b82493a_changing_summary_object_filters_to_.py
@@ -0,0 +1,48 @@
+"""changing summary object filters to reflect boolean type
+
+Revision ID: 53b71b82493a
+Revises: a725ad636622
+Create Date: 2025-04-16 06:21:48.352867
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '53b71b82493a'
+down_revision = 'a725ad636622'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Update row 26 to use boolean eq filter instead of string in_list
+    op.execute("""
+        UPDATE summary 
+        SET object_filters = '{"calc_topup_required": {"eq": {"value": true}}}'
+        WHERE id = 26
+    """)
+    
+    # Update row 27 to use boolean eq filter instead of string in_list
+    op.execute("""
+        UPDATE summary 
+        SET object_filters = '{"calc_individual_exhausted": {"eq": {"value": true}}}'
+        WHERE id = 27
+    """)
+
+
+def downgrade() -> None:
+    # Revert row 26 back to using string in_list filter
+    op.execute("""
+        UPDATE summary 
+        SET object_filters = '{"calc_topup_required": {"in_list": {"value": ["true"]}}}'
+        WHERE id = 26
+    """)
+    
+    # Revert row 27 back to using string in_list filter
+    op.execute("""
+        UPDATE summary 
+        SET object_filters = '{"calc_individual_exhausted": {"in_list": {"value": ["true"]}}}'
+        WHERE id = 27
+    """)
--- tol-portal-ui/app/src/pages/TUMSteps.tsx
+++ tol-portal-ui/app/src/pages/TUMSteps.tsx
@@ -139,7 +139,7 @@ function TUMSteps() {
           and_: {
             'calc_benchling_volume_ul': {'gte': {'value': 0.5}},
             'benchling_extraction_type':{'in_list': {'value': ['dna']}},
-            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0}},
+            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0.0}},
 
             'benchling_tolid.calc_topup_required': {'eq': {'value': true}},
             'benchling_tolid.calc_tolid_actionable': {'eq': {'value': true }},
@@ -192,7 +192,7 @@ function TUMSteps() {
         "benchling_volume_ul": {},
         "benchling_fluidx_id":{},
         "benchling_extraction_type":{},
-        "benchling_sample.sts_labwhere_parentage": {},
+        "benchling_location": {},
         "benchling_species.goat_ploidy": {},
         "benchling_qubit_concentration_ngul": {},
         "benchling_femto_date_code": {},
@@ -215,8 +215,8 @@ function TUMSteps() {
         filter: {
           and_: {
             'calc_benchling_weight_mg': {'gte': {'value': 0.5}},
-            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0}}, 
-            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0}},
+            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0.0}}, 
+            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0.0}},
 
             'benchling_tolid.calc_topup_required': {'eq': {'value': true}},
             'benchling_tolid.calc_tolid_actionable': {'eq': {'value': true }},
@@ -252,9 +252,7 @@ function TUMSteps() {
         "benchling_tolid.id": {
           rename: "ToLID",
         },
-        "uid": {
-          rename: "Tissue Prep ID"
-        },
+        "benchling_eln_tissue_prep_name": {},
         "benchling_tolid.sts_sample_sts_project_union": {},
         "benchling_tissue_prep_fluidx_id":{},
         "benchling_weight_mg":{},
@@ -284,9 +282,9 @@ function TUMSteps() {
         filter: {
           and_: {
             'calc_benchling_remaining_weight': {'gte': {'value': 0.5}},
-            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0}}, 
-            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0}},
-            'calc_tissue_prep_calc_benchling_weight_mg_max': {'lte': {'value': 0}},
+            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0.0}}, 
+            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0.0}},
+            'calc_tissue_prep_calc_benchling_weight_mg_max': {'lte': {'value': 0.0}},
 
             'benchling_tolid.calc_topup_required': {'eq': {'value': true}},
             'benchling_tolid.calc_tolid_actionable': {'eq': {'value': true }},
@@ -351,12 +349,12 @@ function TUMSteps() {
         id: 'sample-remaining-STS-v1',
         filter: {
           and_: {
-            'sts_tissue_remaining': {'gt': {'value': 0}},
+            'sts_tissue_remaining': {'gt': {'value': 0.0}},
             'sts_eln_id': {'exists': {'negate': true }},
-            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0}}, 
-            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0}},
-            'calc_tissue_prep_calc_benchling_weight_mg_max': {'lte': {'value': 0}},
-            'calc_sample_calc_benchling_remaining_weight_max': {'lte': {'value': 0}},
+            'calc_sequencing_request_calc_mlwh_volume_remaining_max': {'lte': {'value': 0.0}}, 
+            'calc_extraction_calc_benchling_volume_ul_dna_max': {'lte': {'value': 0.0}},
+            'calc_tissue_prep_calc_benchling_weight_mg_max': {'lte': {'value': 0.0}},
+            'calc_benchling_remaining_weight': {'lte': {'value': 0.0}},
 
             'sts_tolid.calc_topup_required': {'eq': {'value': true}},
             'sts_tolid.calc_tolid_actionable': {'eq': {'value': true }},
@@ -484,7 +482,7 @@ function TUMSteps() {
         filter: {
           and_: {
             'sts_tolid.calc_individual_available': {'eq': {'value': true}},
-            'sts_tissue_remaining': {'gt': {'value': 0}},
+            'sts_tissue_remaining': {'gt': {'value': 0.0}},
             'sts_eln_id': {'exists': {'negate': true }},
             
             'sts_tolid.calc_topup_required': {'eq': {'value': false}},
