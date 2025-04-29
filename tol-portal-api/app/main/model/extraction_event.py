aa172c33ba2d95fac8fa727d9535db6fa4e921bd
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-04-29 11:27:38 +0100

    New object_events to log action date

--- tol-portal-api/app/main/model/extraction_event.py
+++ tol-portal-api/app/main/model/extraction_event.py
@@ -15,6 +15,8 @@ class ExtractionEvent(Base):
     extraction_id: Mapped[str] = mapped_column(primary_key=True)
     date_abandoned: Mapped[datetime.datetime] = mapped_column()
     abandoned_by: Mapped[str] = mapped_column()
+    date_topup_actioned: Mapped[datetime.datetime] = mapped_column(nullable=True)
+    topup_actioned_by: Mapped[str] = mapped_column(nullable=True)
 
     @classmethod
     def get_id_column_name(cls) -> str:
