f9e5276009d9719b94bf4158cc468716170084e5
Logan Howat <lh31@sanger.ac.uk>
2025-05-14 13:27:04 +0100

    Updating versions and adding alembic migration for type -> grouping

---
+++ tol-portal-api/migrations/alembic/versions/336f53f9c310_changing_type_to_grouping.py
@@ -0,0 +1,32 @@
+"""changing_type_to_grouping
+
+Revision ID: 336f53f9c310
+Revises: a6742154c867
+Create Date: 2025-05-14 11:38:50.641570
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '336f53f9c310'
+down_revision = 'a6742154c867'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("""
+        UPDATE component
+        SET config = jsonb_set(
+            config - 'type',  -- remove the old 'type'
+            '{grouping}',     -- new key
+            to_jsonb(config->'type')  -- reuse the old value
+        )
+        WHERE component_type = 'chart' AND config ? 'type';
+    """)
+
+
+def downgrade() -> None:
+    pass
