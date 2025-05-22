c9a11df695943f4ac1adc8dfb22179f930a9e118
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-05-22 10:47:16 +0100

    TOLP-8788/8310:TUM refinement

--- tol-portal-api/app/main/model/species_event.py
+++ tol-portal-api/app/main/model/species_event.py
@@ -15,6 +15,7 @@ class SpeciesEvent(Base):
     species_id: Mapped[str] = mapped_column(primary_key=True)
     date_marked_for_recollection: Mapped[datetime.datetime] = mapped_column()
     marked_for_recollection_by: Mapped[str] = mapped_column()
+    marked_for_recollection_reason: Mapped[str] = mapped_column(nullable=False, default='PacBio')
 
     @classmethod
     def get_id_column_name(cls) -> str:
