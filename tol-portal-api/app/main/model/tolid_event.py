16df8e505b94dcf41762d0cad5c620c97385f599
Logan Howat <lh31@sanger.ac.uk>
2025-07-30 15:15:24 +0100

    adding the columns to the model

--- tol-portal-api/app/main/model/tolid_event.py
+++ tol-portal-api/app/main/model/tolid_event.py
@@ -17,6 +17,10 @@ class TolidEvent(Base):
     topup_actioned_by: Mapped[str] = mapped_column()
     date_abandoned: Mapped[datetime.datetime] = mapped_column()
     abandoned_by: Mapped[str] = mapped_column()
+    tol_tum_action_count: Mapped[int] = mapped_column(nullable=True)
+    date_sent_to_review: Mapped[datetime.datetime] = mapped_column(nullable=True)
+    sent_to_review_by: Mapped[str] = mapped_column(nullable=True)
+    in_review: Mapped[bool] = mapped_column(nullable=True)
 
     @classmethod
     def get_id_column_name(cls) -> str:
