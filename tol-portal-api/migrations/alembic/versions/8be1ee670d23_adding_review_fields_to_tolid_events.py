2ff611feb8b3e056b6abaa94891dded72f5d65f8
Logan Howat <lh31@sanger.ac.uk>
2025-08-04 10:02:07 +0100

    TOLP-8987: Alembic migration for review action

---
+++ tol-portal-api/migrations/alembic/versions/8be1ee670d23_adding_review_fields_to_tolid_events.py
@@ -0,0 +1,35 @@
+"""adding_review_fields_to_tolid_events
+
+Revision ID: 8be1ee670d23
+Revises: f84d9c259d18
+Create Date: 2025-07-30 10:30:30.212074
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8be1ee670d23'
+down_revision = 'f84d9c259d18'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'tolid_event',
+        sa.Column('in_review', sa.Boolean, nullable=True),
+    )
+    op.add_column(
+        'tolid_event',
+        sa.Column('sent_to_review_by', sa.String(), nullable=True)
+    )
+    op.add_column(
+        'tolid_event',
+        sa.Column('date_sent_to_review', sa.DateTime(), nullable=True)
+    )
+
+
+def downgrade() -> None:
+    pass
