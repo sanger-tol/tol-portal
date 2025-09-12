f9cd499195fb2183cbbb8a16ff0a0f63bb63c5b6
Thomas Assheton <ta21@sanger.ac.uk>
2025-09-12 15:26:36 +0100

    Updated RemoteTable in SpeciesRecollection

--- tol-portal-ui/app/src/pages/SpeciesRecollection.tsx
+++ tol-portal-ui/app/src/pages/SpeciesRecollection.tsx
@@ -28,54 +28,76 @@ function SpeciesRecollection() {
   const table = (
     <RemoteTable
       id="species-recollection-table"
-      defaultSort='sts_scientific_name'
+      defaultSortByAttribute='sts_scientific_name'
       displaySource
+      cellRenderer={{
+        "speciesLink": SpeciesLink,
+      }}
       fields={{
-        "sts_scientific_name": {
-          rename: "Scientific Name",
-          cellRenderer: {
-            element: SpeciesLink,
-            propPointers: {
-              id: 'uid',
-              name: 'sts_scientific_name'
-            }
-          }
+        data: {
+          "sts_scientific_name": {
+            rename: "Scientific Name",
+            cellRenderer: {
+              element: "speciesLink",
+              props: {
+                id: "${uid}",
+                name: "${sts_scientific_name}"
+              },
+            },
+          },
+          "calc_sample_calc_benchling_remaining_weight_sum": {
+            rename: "Tissue Remaining"
+          },
+          "calc_extraction_calc_benchling_volume_ul_sum": {
+            rename: "Volume of Extraction"
+          },
+          "calc_tissue_prep_calc_benchling_weight_mg_sum": {
+            rename: "Tissue Prep Remaining"
+          },
+          "benchling_sequencing_request_lrpacbio_library_remaining_sum": {
+            rename: "Long Read PacBio Library Remaining in SciOps"
+          },
+          "benchling_sequencing_request_benchling_completion_date_hic_min": {
+            rename: "HiC Request Complete"
+          },
+          "benchling_sequencing_request_benchling_completion_date_pacbio_min": {
+            rename: "Pacbio Request Complete"
+          },
+          "benchling_sequencing_request_benchling_completion_date_rnaseq_min": {
+            rename: "RNASeq Request Complete"
+          },
+          "mlwh_run_data_mlwh_run_complete_hic_min": {
+            rename: "HiC Run Complete"
+          },
+          "mlwh_run_data_mlwh_run_complete_pacbio_min": {
+            rename: "PacBio Run Complete"
+          },
+          "mlwh_run_data_mlwh_run_complete_rnaseq_min": {
+            rename: "RNASeq Run Complete"
+          },
+          "sts_sequencing_material_status": {
+            rename: "Material Status"
+          },
+          "sts_sequencing_material_status_updated_at": {
+            rename: "Material Status Updated"
+          },
         },
-        "calc_sample_calc_benchling_remaining_weight_sum": {
-          rename: "Tissue Remaining"
-        },
-        "calc_extraction_calc_benchling_volume_ul_sum": {
-          rename: "Volume of Extraction"
-        },
-        "calc_tissue_prep_calc_benchling_weight_mg_sum": {
-          rename: "Tissue Prep Remaining"
-        },
-        "benchling_sequencing_request_lrpacbio_library_remaining_sum": {
-          rename: "Long Read PacBio Library Remaining in SciOps"
-        },
-        "benchling_sequencing_request_benchling_completion_date_hic_min": {
-          rename: "HiC Request Complete"
-        },
-        "benchling_sequencing_request_benchling_completion_date_pacbio_min": {
-          rename: "Pacbio Request Complete"
-        },
-        "benchling_sequencing_request_benchling_completion_date_rnaseq_min": {
-          rename: "RNASeq Request Complete"
-        },
-        "mlwh_run_data_mlwh_run_complete_hic_min": {
-          rename: "HiC Run Complete"
-        },
-        "mlwh_run_data_mlwh_run_complete_pacbio_min": {
-          rename: "PacBio Run Complete"
-        },
-        "mlwh_run_data_mlwh_run_complete_rnaseq_min": {
-          rename: "RNASeq Run Complete"
-        },
-        "sts_sequencing_material_status": {
-          rename: "Material Status"
-        },
-        "sts_sequencing_material_status_updated_at": {
-          rename: "Material Status Updated"
+        order: {
+          active: [
+            "sts_scientific_name",
+            "calc_sample_calc_benchling_remaining_weight_sum",
+            "calc_extraction_calc_benchling_volume_ul_sum",
+            "calc_tissue_prep_calc_benchling_weight_mg_sum",
+            "benchling_sequencing_request_lrpacbio_library_remaining_sum",
+            "benchling_sequencing_request_benchling_completion_date_hic_min",
+            "benchling_sequencing_request_benchling_completion_date_pacbio_min",
+            "benchling_sequencing_request_benchling_completion_date_rnaseq_min",
+            "mlwh_run_data_mlwh_run_complete_hic_min",
+            "mlwh_run_data_mlwh_run_complete_pacbio_min",
+            "mlwh_run_data_mlwh_run_complete_rnaseq_min",
+            "sts_sequencing_material_status",
+            "sts_sequencing_material_status_updated_at",
+          ],
         },
       }}
       {...speciesRecollection}
