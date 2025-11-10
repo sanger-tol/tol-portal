a68c0509f5012789cedb21a46038ebe9c5862f42
Kiernan Harding <kh16@sanger.ac.uk>
2025-11-10 11:57:01 +0000

    update alembic

--- tol-portal-api/migrations/alembic/versions/cd6c3a0ae19a_datasource_to_data_source_instance_fk.py
+++ tol-portal-api/migrations/alembic/versions/cd6c3a0ae19a_datasource_to_data_source_instance_fk.py
@@ -1,7 +1,7 @@
 """datasource_to_data_source_instance_fk
 
 Revision ID: cd6c3a0ae19a
-Revises: 76284ef5166d
+Revises: f6f142c1a7bb
 Create Date: 2025-09-22 13:07:57.229405
 
 """
@@ -13,7 +13,7 @@ from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = 'cd6c3a0ae19a'
-down_revision = '76284ef5166d'
+down_revision = 'f6f142c1a7bb'
 branch_labels = None
 depends_on = None
 
@@ -21,19 +21,66 @@ depends_on = None
 def upgrade() -> None:
     conn = op.get_bind()
 
+    # Create new `api_details` field to data_source_instance table
+    op.add_column('data_source_instance', sa.Column('ui_api_details', JSONB, nullable=True))
+
+    # Remove autoincrement from id column and change to string type
+    op.alter_column('data_source_instance', 'id', 
+                   type_=sa.String(),
+                   autoincrement=False,
+                   nullable=False)
+
+    # Update all id fields to use name values
+    conn.execute(
+        text("""
+        UPDATE data_source_instance
+        SET id = name
+        """)
+    )
+
+    # Drop the name column
+    op.drop_column('data_source_instance', 'name')
+
+    # Update api_details for 'tol_production' data source instance
+    conn.execute(
+        text("""
+        UPDATE data_source_instance
+        SET ui_api_details = '{"url": "https://portal.tol.sanger.ac.uk", "apiPath": "/api/v1", "apiDataPath": "/data", "dataspace": "tol_production"}'
+        WHERE id = 'tol_production'
+        """)
+    )
+
+    # Update tolqc id
+    conn.execute(
+        text("""
+        UPDATE data_source_instance
+        SET id = 'tolqc_production'
+        WHERE id = 'tolqc'
+        """)
+    )
+
+    # Update api_details for 'tolqc_production' data source instance
+    conn.execute(
+        text("""
+        UPDATE data_source_instance
+        SET ui_api_details = '{"url": "https://portal.tol.sanger.ac.uk", "apiPath": "/api/v1", "apiDataPath": "/data", "dataspace": "tolqc_production"}'
+        WHERE id = 'tolqc_production'
+        """)
+    )
+
     # Remove `datasource` fields
     op.drop_column('component', 'datasource')
     op.drop_column('zone', 'datasource')
 
     # Create new `data_source_instance_id` fields in their places,
     # with foreign keys linking to the `data_source_instance` table
-    op.add_column('component', sa.Column('data_source_instance_id', sa.Integer, nullable=False))
+    op.add_column('component', sa.Column('data_source_instance_id', sa.Integer, nullable=True))
     op.create_foreign_key(
         'fk_component_data_source_instance',
         'component', 'data_source_instance',
         ['data_source_instance_id'], ['id']
     )
-    op.add_column('zone', sa.Column('data_source_instance_id', sa.Integer, nullable=False))
+    op.add_column('zone', sa.Column('data_source_instance_id', sa.Integer, nullable=True))
     op.create_foreign_key(
         'fk_zone_data_source_instance',
         'zone', 'data_source_instance',
@@ -54,14 +101,10 @@ def upgrade() -> None:
         """)
     )
 
+    # Set columns as NOT NULL now that data is populated
+    op.alter_column('component', 'data_source_instance_id', nullable=False)
+    op.alter_column('zone', 'data_source_instance_id', nullable=False)
+
 
 def downgrade() -> None:
-    # Remove `data_source_instance_id` fields from `component` and `zone` tables
-    op.drop_constraint('fk_component_data_source_instance', 'component')
-    op.drop_column('component', 'data_source_instance_id')
-    op.drop_constraint('fk_zone_data_source_instance', 'zone')
-    op.drop_column('zone', 'data_source_instance_id')
-
-    # Replace back old `datasource` field into these tables
-    op.add_column('component', sa.Column('datasource', JSONB, nullable=False, server_default='{"api_prefix": "data/tol_production"}'))
-    op.add_column('zone', sa.Column('datasource', JSONB, nullable=False, server_default='{"api_prefix": "data/tol_production"}'))
+    pass
