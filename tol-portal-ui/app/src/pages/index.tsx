80548269071a6807dfd2e84ecbbe02583153ef35
Luke Wilson <lw23@sanger.ac.uk>
2025-11-24 14:26:55 +0000

    ui partially implemented

--- tol-portal-ui/app/src/pages/index.tsx
+++ tol-portal-ui/app/src/pages/index.tsx
@@ -9,7 +9,7 @@ export { default as Loaders } from "./Loaders";
 export { default as Attributes } from "./Attributes";
 export { default as Manifests } from "./Manifests";
 export { default as Species } from "./Species";
-export { default as SpeciesDetail } from "./SpeciesDetail"; 
+export { default as SpeciesDetail } from "./SpeciesDetail";
 export { default as Specimens } from "./Specimens";
 export { default as Samples } from "./Samples";
 export { default as Samplesets } from "./Samplesets";
@@ -22,7 +22,8 @@ export { default as Curations } from "./Curations";
 export { default as TUM } from "./TUM";
 export { default as SpeciesRecollection } from "./SpeciesRecollection";
 export { default as SamplesStuck } from "./SamplesStuck";
-export { default as ProjectManagement } from "./ProjectManagement"
-export { default as TUMSteps } from "./TUMSteps"
+export { default as ProjectManagement } from "./ProjectManagement";
+export { default as TUMSteps } from "./TUMSteps";
 export { default as ARAReview } from "./ARAReview";
 export { default as SampleSelection } from "./SampleSelection";
+export { default as ManifestValidation } from "./ManifestValidation";
