acca3df41e0938b129a858f00d974f83883be894
Luke Wilson <lw23@sanger.ac.uk>
2025-12-03 15:25:28 +0000

    work for TOLP-9350

--- tol-portal-ui/app/src/pages/ManifestValidation.tsx
+++ tol-portal-ui/app/src/pages/ManifestValidation.tsx
@@ -14,7 +14,7 @@ import {
 } from "@tol/tol-ui";
 
 const VALIDATION_CONFIG = {
-  s3_url: "tol-sample-manifests", // TODO: change to correct bucket depending on pipeline_id
+  s3_url: "lw23-scratch", // TODO: change to correct bucket depending on pipeline_id
   pipeline_id: 1, // TODO: Allow users to select pipeline to run
   destination: "portal",
 };
