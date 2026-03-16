1b677fe472a2983222520df133f38e6f62fd9a02
Thomas Assheton <ta21@sanger.ac.uk>
2026-03-16 10:11:59 +0000

    Create migration

---
+++ tol-portal-api/migrations/alembic/versions/452f817312e7_rename_prefix_to_provenance.py
@@ -0,0 +1,24 @@
+"""Rename prefix to provenance
+
+Revision ID: 452f817312e7
+Revises: f0cf5e525b9e
+Create Date: 2026-03-16 10:10:26.640983
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '452f817312e7'
+down_revision = 'f0cf5e525b9e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
