5d5d076c5f3b9a54917727780ee51f8434c8a95c
Andrew Varley <av10@sanger.ac.uk>
2026-04-02 16:05:55 +0100

    TOLP-9692 Add loader.config column

---
+++ tol-portal-api/migrations/alembic/versions/4428f2cf7741_add_data_source_instance_config_column.py
@@ -0,0 +1,50 @@
+"""add data_source_instance.config column
+
+Revision ID: 4428f2cf7741
+Revises: fbbdb8cdb736
+Create Date: 2026-03-31 14:27:32.847042
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4428f2cf7741'
+down_revision = 'fbbdb8cdb736'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'loader',
+        sa.Column('config', sa.JSON(), nullable=False, server_default='{}')
+    )
+    op.execute(
+        sa.text(
+            "UPDATE loader"
+            " SET config = json_build_object("
+            "    'module', 'tol.flows.converters',"
+            "    'class_name', convert_class,"
+            "    'is_validator', false"
+            " )"
+            " WHERE convert_class IS NOT NULL"
+        )
+    )
+
+    op.execute(
+        sa.text(
+            "UPDATE loader"
+            " SET config = json_build_object("
+            "    'module', 'tol.core',"
+            "    'class_name', 'DefaultDataObjectToDataObjectConverter',"
+            "    'is_validator', false"
+            " )"
+            " WHERE convert_class IS NULL"
+        )
+    )
+
+
+def downgrade() -> None:
+    op.drop_column('loader', 'config')
