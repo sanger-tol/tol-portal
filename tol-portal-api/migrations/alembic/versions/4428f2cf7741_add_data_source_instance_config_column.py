4fd4368a4ed3d74e99c07e62ebb5361025271095
Andrew Varley <av10@sanger.ac.uk>
2026-04-02 13:00:05 +0100

    Updated alembic migration

--- tol-portal-api/migrations/alembic/versions/4428f2cf7741_add_data_source_instance_config_column.py
+++ tol-portal-api/migrations/alembic/versions/4428f2cf7741_add_data_source_instance_config_column.py
@@ -1,7 +1,7 @@
 """add data_source_instance.config column
 
 Revision ID: 4428f2cf7741
-Revises: 81c01adb8680
+Revises: fbbdb8cdb736
 Create Date: 2026-03-31 14:27:32.847042
 
 """
@@ -11,19 +11,19 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '4428f2cf7741'
-down_revision = '81c01adb8680'
+down_revision = 'fbbdb8cdb736'
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
     op.add_column(
-        'data_source_instance',
+        'loader',
         sa.Column('config', sa.JSON(), nullable=False, server_default='{}')
     )
     op.execute(
         sa.text(
-            "UPDATE data_source_instance"
+            "UPDATE loader"
             " SET config = json_build_object("
             "    'module', 'tol.flows.converters',"
             "    'class_name', convert_class,"
@@ -35,10 +35,10 @@ def upgrade() -> None:
 
     op.execute(
         sa.text(
-            "UPDATE data_source_instance"
+            "UPDATE loader"
             " SET config = json_build_object("
             "    'module', 'tol.core',"
-            "    'class_name', 'DefaultDataObjectToDataObjectConverter,"
+            "    'class_name', 'DefaultDataObjectToDataObjectConverter',"
             "    'is_validator', false"
             " )"
             " WHERE convert_class IS NULL"
@@ -47,4 +47,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.drop_column('data_source_instance', 'config')
+    op.drop_column('loader', 'config')
