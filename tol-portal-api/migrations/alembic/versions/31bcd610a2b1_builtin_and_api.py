43eb7f7c21661da05c91ccd1680fda70d1a17a1a
Andrew Varley <av10@sanger.ac.uk>
2026-09-15 15:42:02 +0100

    Alembic correction

--- tol-portal-api/migrations/alembic/versions/31bcd610a2b1_builtin_and_api.py
+++ tol-portal-api/migrations/alembic/versions/31bcd610a2b1_builtin_and_api.py
@@ -1,7 +1,7 @@
 """builtin and api
 
 Revision ID: 31bcd610a2b1
-Revises: 5f3a925dad6b
+Revises: b77923994607
 Create Date: 2026-06-12 16:58:18.408837
 
 """
@@ -11,14 +11,20 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '31bcd610a2b1'
-down_revision = '5f3a925dad6b'
+down_revision = 'b77923994607'
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.alter_column('data_source_instance', 'builtin_name', new_column_name='direct_name')
-    op.alter_column('data_source_instance', 'kwargs', new_column_name='direct_kwargs')
+    op.add_column(
+        'data_source_instance',
+        sa.Column('direct_name', sa.String(), nullable=True)
+    )
+    op.add_column(
+        'data_source_instance',
+        sa.Column('direct_kwargs', sa.JSON(), nullable=True)
+    )
     op.add_column(
         'data_source_instance',
         sa.Column('api_name', sa.String(), nullable=True)
@@ -26,6 +32,13 @@ def upgrade() -> None:
     op.add_column(
         'data_source_instance',
         sa.Column('api_kwargs', sa.JSON(), nullable=True)
+    )
+
+    op.execute(
+        """
+        UPDATE data_source_instance
+        SET direct_name = name, direct_kwargs = kwargs
+        """
     )
 
     op.execute(
@@ -42,7 +55,7 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.alter_column('data_source_instance', 'direct_name', new_column_name='builtin_name')
-    op.alter_column('data_source_instance', 'direct_kwargs', new_column_name='kwargs')
+    op.drop_column('data_source_instance', 'direct_name')
+    op.drop_column('data_source_instance', 'direct_kwargs')
     op.drop_column('data_source_instance', 'api_name')
     op.drop_column('data_source_instance', 'api_kwargs')
